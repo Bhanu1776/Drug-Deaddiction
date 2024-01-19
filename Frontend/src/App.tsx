8b3b40f3b562012704df537e62f5bb92d614d943
Osama <shaikhosama499@gmail.com>
2024-01-19 15:48:47 +0530

    chore(appp): important component created for landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,21 +1,16 @@
-import CountBtn from '@/components/CountBtn';
-import ReactSVG from '@/assets/react.svg';
-import { Badge } from '@/components/ui/badge';
-
+import Carousel from "./pages/Home/components/HomeCarousel";
+import Services from "./pages/Home/components/Services";
+import Treatment from "./pages/Home/components/Treatment";
+import Activities from "./pages/Home/components/Activities";
+import Footer from "./pages/Home/components/Footer";
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={'/vite.svg'} alt="Vite Logo" className="w-32" />
-        </div>
-        <a href="https://ui.shadcn.com" rel="noopener noreferrer nofollow" target="_blank">
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
-      </div>
+    <main className="flex flex-col items-center h-screen mx-48">
+    <Carousel />
+    <Services />
+    <Activities />
+    <Treatment />
+    <Footer />
     </main>
   );
 }
---
+++ Frontend/src/pages/Home/components/HomeCarousel.tsx
@@ -0,0 +1,50 @@
+// import React from 'react'
+import img1 from "/public/img1.jpg"
+import img2 from "/public/img2.jpg"
+// import img3 from "/public/img3.jpg"
+import img4 from "/public/img4.jpg"
+import img5 from "/public/img5.jpg"
+
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+const HomeCarousel = () => {
+  return (
+    <>
+     <Carousel className="rounded-lg mt-28">
+  <CarouselContent  >
+    <CarouselItem  >
+      
+    <img src={img2} alt="" className="relative rounded-lg"/>
+      <span className="absolute flex flex-col text-5xl font-semibold text-white right-10 bottom-10 "><span className="text-blue-500">Near-Painless </span>Drug De-Addiction</span>
+    </CarouselItem>
+    <CarouselItem>
+    <img src={img1} alt="" className="rounded-lg " />
+     
+    </CarouselItem>
+    {/* <CarouselItem>
+      <img src={img3} alt="" />
+
+    </CarouselItem> */}
+    <CarouselItem >
+      <img src={img4} alt="" className="rounded-lg " />
+
+    </CarouselItem>
+    <CarouselItem>
+      <img src={img5} alt="" className="rounded-lg "/>
+
+    </CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel></>
+  )
+}
+
+export default HomeCarousel
+
