d4f0e788c57a09cad8a3d1f5c86cff18c62e6575
Osama <shaikhosama499@gmail.com>
2024-01-19 15:45:58 +0530

    chore(app): static data added for homepage

---
+++ Frontend/src/data/HomePageData.ts
@@ -0,0 +1,125 @@
+export const drugData = [
+{
+id:1,
+heading:"What is Drug Addiction?",
+
+data:[
+    {
+id:1,
+
+subheading:"",
+para:"Addiction, a disease impacting both the brain and behavior, compels individuals to use drugs despite the potential harm. Seeking early treatment for drug addiction, also known as substance use disorder, increases the likelihood of avoiding severe consequences."
+},
+    {
+id:2,
+
+subheading:"",
+para:"Drug addiction extends beyond illicit substances like heroin or cocaine; legal substances such as alcohol, nicotine, sleep aids, anti-anxiety medications, and even prescription or illicitly obtained opioids can lead to addiction. The United States is facing an epidemic of opioid addiction, with opioids contributing to two-thirds of all drug overdose deaths in 2018.‍"
+},
+{
+    id:3,
+    
+    subheading:"",
+    para:"Initially, one may choose to use a drug due to the pleasurable effects it produces, believing they can maintain control over usage. However, drugs gradually alter brain function, resulting in enduring physical changes. These changes undermine self-control and can lead to harmful behaviors."
+}
+]
+},
+{
+    id:2,
+    heading:"De-addiction Service",
+    data:[{
+        id:1,
+        
+        subheading:"",
+        para:"De-addiction counseling encompasses a process aimed at overcoming harmful dependencies on substances such as alcohol, nicotine, and potent psychotropic drugs. It extends beyond addressing substance abuse alone, also addressing addictive behaviors like excessive gaming, the use of electronic gadgets, sex, and masturbation. Individuals grappling with any form of addiction may encounter difficulties in carrying out daily activities and commonly experience feelings of depression and anxiety."
+    },{
+        id:2,
+        
+        subheading:"",
+        para:"De-addiction counseling plays a vital role in fostering awareness of the negative consequences associated with addiction, while equipping individuals with effective strategies for managing their issues and attaining self-control."}]
+    },
+    {
+        id:3,
+        heading:"When Do You Need De-addiction Service?",
+        data:[{
+            id:1,
+            subheading:"Physical damage or disease:",
+            para:"Smoking substances such as tobacco or weed can result in addiction and contribute to respiratory system harm. Similarly, injecting drugs can lead to damage in your veins and arteries, which can ultimately result in the development of incurable diseases."
+        },
+        {
+            id:2,
+            subheading:"Withdrawal Symptoms:",
+            para:"Intense addiction can trigger various symptoms, including strong cravings, trembling, and, in extreme cases, a potential for violent behavior."
+        },
+        
+        {
+            id:3,
+            
+            subheading:"Feeling of helplessness:",
+            para:"Once addiction takes hold, a sense of helplessness can arise, causing individuals to exhibit uncharacteristic behavior. They may abandon their once-enjoyed hobbies and activities, prioritizing the acquisition of their substance of choice, often resorting to sacrificing other aspects of their lives in order to obtain it."
+        },
+        {
+            id:4,
+            
+            subheading:"Financial difficulties:",
+            para:"Addiction can lead to a significant depletion of financial resources as the individual spends all their money on acquiring the addictive substances. The lack of control over expenses becomes apparent, with individuals sometimes prioritizing their addiction over essential expenditures, jeopardizing the stability of their supply."
+        },
+        {
+            id:5,
+            
+            subheading:"Excess consumption of substances:",
+            para:"An excessive consumption of drugs, alcohol, or tobacco can escalate to physical abuse, particularly prevalent among individuals struggling with alcohol addiction."
+        }]
+    },
+    {
+        id:4,
+        heading:"Why Medication is Needed for Addiction Treatment",
+        subheading:"",
+        data:[
+        {
+        id:1,
+        
+        subheading:"",
+        para:"Medications are a necessity during the period of withdrawal. Long term abuse of alcohol has a sedative effect on the body, which is why when an individual suddenly withdraws from alcohol there is a phase of excitability. This phase can be deadly if not for the use of medication."
+        },
+        {
+        id:2,
+        
+        subheading:"",
+        para:"The phase of excitability can cause a range of symptoms like a spike in blood pressure and rapid heart rate, which can eventually lead to delirium tremens, tremors and even delirium. Extreme cases. Alcohol withdrawal that is left untreated can lead to seizures, heart attack or even strokes."
+        }]
+}
+]
+
+export const treatment = [{
+id:1,
+heading:"6 Treatments for Addiction That Are Proven Successful",
+data:[{
+id:1,
+heading:"Detoxification",
+para:"Detoxification is often the first step in addiction treatment. It involves safely managing withdrawal symptoms when an individual stops using drugs or alcohol. Detoxification can be done in an inpatient or outpatient setting and may include medical supervision and medications to alleviate withdrawal symptoms."
+},
+{
+    id:2,
+    heading:"Cognitive Behavioral Therapy",
+    para:"CBT is a widely used therapeutic approach for addiction treatment. It focuses on identifying and modifying the unhealthy thoughts, beliefs, and behaviors that contribute to substance abuse. CBT helps individuals develop coping skills, manage cravings, and prevent relapse by addressing underlying psychological factors."
+    },{
+        id:3,
+        heading:"Rational Emotive Behavior Therapy",
+        para:"REBT is a form of cognitive therapy that helps individuals challenge and change irrational beliefs and thoughts that contribute to addictive behaviors. It emphasizes the connection between thoughts, emotions, and behaviors, and aims to promote rational thinking and emotional regulation."
+        },{
+            id:4,
+            heading:"Contingency Management",
+            para:"Contingency management is a behavioral therapy approach that uses positive reinforcement to encourage and reward individuals for abstaining from substance abuse. It involves providing tangible incentives such as vouchers, privileges, or prizes when desired behaviors (such as drug-free urine tests) are achieved."
+            },{
+                id:5,
+                heading:"Treatment With Medication",
+                para:"Medications can be an essential component of addiction treatment, particularly for certain substances like opioids, alcohol, or tobacco. Medications may be used to manage withdrawal symptoms, reduce cravings, and prevent relapse. Examples of medications used in addiction treatment include methadone, buprenorphine, naltrexone, acamprosate, and nicotine replacement therapy."
+                },{
+                    id:6,
+                    heading:"12-step Facilitation",
+                    para:"This treatment approach is based on the principles of Alcoholics Anonymous (AA) and similar 12-step programs. It involves attending meetings, working through the 12 steps, and connecting with a supportive community of individuals in recovery. 12-step facilitation focuses on accepting powerlessness over addiction, surrendering to a higher power, and seeking support from peers."
+                    }
+
+]
+}]
