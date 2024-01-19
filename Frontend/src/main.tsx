1084fa34652873705ff71fdb445fc9e017557c4f
Bhanu1776 <sunkabhanu281202@gmail.com>
2024-01-19 13:43:47 +0530

    chore: setup tanstack query

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,11 +4,18 @@ import App from './App.tsx';
 import '@/styles/globals.css';
 import store from '@/store/store.ts';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <App />
     </Provider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
   </React.StrictMode>
 );
